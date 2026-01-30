94ed9819eccab9fd8dfbbf43a6b433e8bd45b3d5
Hermit <persona_gazer@outlook.com>
2026-01-30 15:05:35 +0900

    MOD: Make comment more readable.

--- lua/core/basic.lua
+++ lua/core/basic.lua
@@ -16,3 +16,11 @@ vim.opt.smartcase = true
 vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 99
 vim.opt.foldenable = true
+
+-- enable edit from outside
+vim.opt.autoread = true
+vim.opt.updatetime = 200
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+  pattern = "*",
+  command = "silent! checktime",
+})
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,7 +1,12 @@
 return {
     "folke/tokyonight.nvim",
     opts = {
-        style = "moon"
+        style = "moon",
+        on_highlights = function(hl, c)
+            hl.Comment = { fg = c.orange, italic = true }
+            hl["@comment"] = { link = "Comment" }
+            hl["@lsp.type.comment"] = { link = "Comment" }
+        end,
     },
     config = function (_, opts)
         require("tokyonight").setup(opts)
